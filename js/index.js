609be2e23393638cc64b69831dc38cbf25f6f2d9
Cecy98 <ceciliagtzceja@gmail.com>
2023-01-03 18:10:00 -0800

    Updating before starting HW 20
    Just a regular update.

--- js/index.js
+++ js/index.js
@@ -3,5 +3,5 @@ console.log("The index.js file is loaded correctly!");
 
 function myFunction() {
   document.getElementById("downloadResume").innerHTML = "Thank you!";
-  document.removeElementById("downloadResume").innerHTML;
+
 }
